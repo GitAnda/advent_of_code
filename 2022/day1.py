16f7596b0d8417dbdf686c0ddc9279db0e1cc20a
GitAnda <andagfilipovic@gmail.com>
2022-12-01 22:24:23 +0100

    simplify import of data

--- 2022/day1.py
+++ 2022/day1.py
@@ -1,14 +1,10 @@
-from aocd import data
-from pathlib import Path
+import input
+    
+def parse_data():
+    data = input.retrieve(__file__)
+    return [sum([int(snack) for snack in elf.split("\n")]) for elf in data.split("\n\n")]
 
-txtfile =  Path(__file__).parent / (Path(__file__).stem + ".txt")
-if not txtfile.exists():
-    with open(txtfile, 'w') as f:
-        f.write(data)
-
-with open(txtfile) as f:
-    data = [sum([int(snack) for snack in elf.split("\n")]) for elf in f.read().split("\n\n")]
-elfs = sorted(data, reverse=True)
+elfs = sorted(parse_data(), reverse=True)
 
 print(f"Part 1: {elfs[0]}")
 print(f"Part 2: {sum(elfs[0:3])}")
