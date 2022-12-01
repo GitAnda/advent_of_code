fd3f9d96fdb82a71e42397a56df42cdaf6705072
GitAnda <andagfilipovic@gmail.com>
2022-12-01 08:18:37 +0100

    add rust solution day 1

---
+++ 2022/rust/src/day1.rs
@@ -0,0 +1,24 @@
+const INPUT: &str = include_str!("day1.txt");
+
+fn input() -> Vec<Vec<u32>> {
+    INPUT
+        .split("\r\n\r\n")
+        .map(|line| {
+            line.split("\r\n")
+                .map(|value| value.parse().unwrap())
+                .collect::<Vec<u32>>()
+        })
+        .collect::<Vec<Vec<u32>>>()
+}
+
+#[test]
+fn part1() {
+    let mut elfs = input()
+        .iter()
+        .map(|elf| elf.iter().sum())
+        .collect::<Vec<u32>>();
+    elfs.sort();
+    elfs.reverse();
+    println!("Part 1: {:?}", elfs.first().unwrap());
+    println!("Part 1: {:?}", elfs[0] + elfs[1] + elfs[2]);
+}
