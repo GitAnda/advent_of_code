4a5050f048db84fe9b529f7882dfe09301c701d2
GitAnda <andagfilipovic@gmail.com>
2022-12-03 06:22:23 +0100

    solve day 3

--- 2022/day3.py
+++ 2022/day3.py
@@ -4,7 +4,15 @@ def parse_data():
     data = input.retrieve(__file__)
     return data
 
-elfs = sorted(parse_data(), reverse=True)
+def priority(char):
+    if char.islower():
+        return ord(char) - 97 + 1
+    else:
+        return ord(char) - 65 + 27
+    
+data = parse_data().split()
+shared = [(set(d[:len(d)//2]) & set(d[len(d)//2:])).pop() for d in data]
+print(f"Part 1: {sum([priority(s) for s in shared])}")
 
-print(f"Part 1: {elfs[0]}")
-print(f"Part 2: {sum(elfs[0:3])}")
+shared = [(set(data[i]) & set(data[i+1]) & set(data[i+2])).pop() for i in range(0, len(data), 3)]
+print(f"Part 2: {sum([priority(s) for s in shared])}")
