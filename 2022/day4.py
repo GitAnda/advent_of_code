febf2f4e080b5cf64e018676da546a01b6d0a690
GitAnda <andagfilipovic@gmail.com>
2022-12-04 06:21:46 +0100

    solve day 4

---
+++ 2022/day4.py
@@ -0,0 +1,27 @@
+import input
+    
+def parse_data():
+    data = [[tuple([int(y) for y in x.split('-')]) for x in i.split(',')] for i in input.retrieve(__file__).split()]
+    return data
+
+def contains(pair1, pair2):
+    x1, y1 = pair1
+    x2, y2 = pair2
+    if x1 <= x2 and y2 <= y1:
+        return True
+    return False
+
+def overlaps(pair1, pair2):
+    x1, y1 = pair1
+    x2, y2 = pair2
+    if y1 < x2 or y2 < x1:
+        return False
+    return True
+    
+data = parse_data()
+contained = [contains(d[0], d[1]) or contains(d[1], d[0]) for d in data]
+print(f"Part 1: {sum(contained)}")
+
+overlap = [overlaps(d[0], d[1]) for d in data]
+print(overlap)
+print(f"Part 2: {sum(overlap)}")
