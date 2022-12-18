1a0fee00b03a20462c762970a26a6ce0222af538
GitAnda <andagfilipovic@gmail.com>
2022-12-18 12:38:40 +0100

    solve day 17

---
+++ 2022/day18.py
@@ -0,0 +1,15 @@
+import input
+    
+def parse_data():
+    data = input.retrieve(__file__).split('\n')
+    return data
+
+data = parse_data()
+print(data)
+
+res = 0
+print(f"Part 1: {res}")
+
+res = 0
+print(f"Part 2: {res}")
+
