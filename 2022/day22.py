d1181a9f75d663a6368945400fc3cd0a72a06edf
Knol, Anda <a.knol@nfi.nl>
2022-12-22 14:03:29 +0100

    add solution day 22

--- 2022/day22.py
+++ 2022/day22.py
@@ -1,8 +1,11 @@
 import input
-from functools import lru_cache
-    
+
+# x = open("2022/day22.txt", "r").read()
+# grid, ins = x.split("\n\n")
+
 def parse_data():
     m, r = input.retrieve(__file__).split('\n\n')
+    # m, r = open("2022\day22.txt").read().split("\n\n")
     
     wall = set()
     path = set()
@@ -20,26 +23,33 @@ def parse_data():
 WALL, PATH, ROUTE = parse_data()
 WRAP = {}
 for d in range(50):
-    WRAP[(3, 51 + d, 1)] = (0, 1, 151 + d)
-    WRAP[(2, 1, 151 + d)] = (1, 51 + d, 1)
-    
-    WRAP[(2, 51, 1 + d)] = (0, 1, 150 - d)
-    WRAP[(2, 1, 150 - d)] = (0, 51, 1 + d)
-    
-    WRAP[(2, 52, 51 + d)] = (1, 1 + d, 101)
-    WRAP[(3, 1 + d, 101)] = (0, 52, 51 + d)
-    
-    WRAP[(3, 101 + d, 1)] = (3, 1 + d, 200)
-    WRAP[(3, 1 + d, 200)] = (3, 101 + d, 1)
-    
-    WRAP[(0, 151, 1 + d)] = (2, 100, 150 - d)
-    WRAP[(0, 100, 150 - d)] = (2, 151, 1 + d)
-    
-    WRAP[(1, 52 + d, 150)] = (2, 50, 151 + d)
-    WRAP[(0, 50, 151 + d)] = (3, 52 + d, 150)
-    
-    WRAP[(1, 102 + d, 50)] = (2, 100, 51 + d)
-    WRAP[(0, 100, 51 + d)] = (3, 102 + d, 50)
+    (fa, xa, ya), (fb, xb, yb) = (3, 51, 1), (0, 1, 151)
+    WRAP[(fa, xa+d, ya)] = (fb, xb, yb+d)
+    WRAP[((fb-2)%4, xb, yb+d)]= ((fa-2)%4, xa+d, ya)
+
+    (fa, xa, ya), (fb, xb, yb) = (3, 101, 1), (3, 1, 200)
+    WRAP[(fa, xa+d, ya)] = (fb, xb+d, yb)
+    WRAP[((fb-2)%4, xb+d, yb)]= ((fa-2)%4, xa+d, ya)
+
+    (fa, xa, ya), (fb, xb, yb) = (1, 51, 150), (2, 50, 151)
+    WRAP[(fa, xa+d, ya)] = (fb, xb, yb+d)
+    WRAP[((fb-2)%4, xb, yb+d)]= ((fa-2)%4, xa+d, ya)
+
+    (fa, xa, ya), (fb, xb, yb) = (2, 51, 100), (1, 50, 101)
+    WRAP[(fa, xa, ya-d)] = (fb, xb-d, yb)
+    WRAP[((fb-2)%4, xb-d, yb)]= ((fa-2)%4, xa, ya-d)
+
+    (fa, xa, ya), (fb, xb, yb) = (1, 101, 50), (2, 100, 51)
+    WRAP[(fa, xa+d, ya)] = (fb, xb, yb+d)
+    WRAP[((fb-2)%4, xb, yb+d)]= ((fa-2)%4, xa+d, ya)
+
+    (fa, xa, ya), (fb, xb, yb) = (2, 51, 1), (0, 1, 150)
+    WRAP[(fa, xa, ya+d)] = (fb, xb, yb-d)
+    WRAP[((fb-2)%4, xb, yb-d)]= ((fa-2)%4, xa, ya+d)
+
+    (fa, xa, ya), (fb, xb, yb) = (0, 150, 1), (2, 100, 150)
+    WRAP[(fa, xa, ya+d)] = (fb, xb, yb-d)
+    WRAP[((fb-2)%4, xb, yb-d)]= ((fa-2)%4, xa, ya+d)
 
 def wrap_pos(p, dx, dy):
     while True:
@@ -48,61 +58,57 @@ def wrap_pos(p, dx, dy):
             return (x, y)
         p = (x - dx, y - dy)
 
+def get_dxy(f):
+    if f == 0:
+        return 1, 0
+    elif f == 1:
+        return 0, 1
+    elif f == 2:
+        return -1, 0
+    elif f == 3:
+        return 0, -1
+
+def next_pos(r, face, x, y, cube):
+    if r == 'R':
+        face = (face + 1) % 4
+    elif r == 'L':
+        face = (face - 1) % 4
+    else:
+        
+        for _ in range(r):
+            dx, dy = get_dxy(face)
+            
+            new_pos = (x + dx, y + dy)
+            nf = face
+            if new_pos not in PATH | WALL:
+                if cube:
+                    nf, nx, ny = WRAP[(face, x, y)]
+                    new_pos = nx, ny
+                else:
+                    new_pos = wrap_pos(new_pos, dx, dy)
+                
+            if new_pos in WALL:
+                break
+            
+            x, y = new_pos
+            face = nf
+
+    return face, x, y
 
 def walk(cube=False):
-    pos = min([(x,y) for x,y in PATH if y == 1])
+    x, y = min([(x,y) for x,y in PATH if y == 1])
     face = 0
 
     for r in ROUTE:
-        if r == 'R':
-            face = (face + 1) % 4
-            # print("turn right:", face)
-        elif r == 'L':
-            face = (face - 1) % 4
-            # print("turn left:", face)
+        face, x, y = next_pos(r, face, x, y, cube)
             
-        else:
-            for _ in range(r):
-                x, y = pos
-                # print("Current position", pos)
-                
-                if face == 0:
-                    dx, dy = 1, 0
-                elif face == 1:
-                    dx, dy = 0, 1
-                elif face == 2:
-                    dx, dy = -1, 0
-                elif face == 3:
-                    dx, dy = 0, -1
-                
-                new_pos = (x + dx, y + dy)
-                # print(new_pos)
-                if new_pos not in PATH | WALL:
-                    # print("New pos not in map!")
-                    if cube:
-                        face, a, b = WRAP[(face, *new_pos)]
-                        new_pos = a, b
-                    else:
-                        new_pos = wrap_pos(new_pos, dx, dy)
-                    
-                    
-                    # print(new_pos)
-                    
-                if new_pos in WALL:
-                    # print("New pos is wall! Stay at", pos)
-                    break
-                
-                pos = new_pos
-                # print("Take a step to", pos)
-            # print(pos)
-            
-    x, y = pos
-    return 1000 * y + 4 * x + face  
-            
-# res = walk()
-# print(f"Part 1: {res}")
+    return 1000 * y + 4 * x + face 
 
-
+            
+res = walk()
+print(f"Part 1: {res}")
 
 res = walk(cube=True)
 print(f"Part 2: {res}")
+
+
